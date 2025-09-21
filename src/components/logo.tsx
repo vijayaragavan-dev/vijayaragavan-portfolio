5fd29c9e7d9ae61d752ecefb5c120d7ad6e1cc52
Vijayaragavan <vijayaragavanproject1@gmail.com>
2025-09-21 08:46:25 +0000

    when user refresh the page or load the page it should come feom bottom t

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,15 @@
+import { AnimateInView } from './animate-in-view';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export function Logo() {
   return (
-    <Link href="/" className="group">
-      <h1 className={cn('text-2xl font-bold tracking-tighter text-foreground transition-all duration-300 ease-in-out group-hover:text-glow')}>
-        Vijayaragavan
-      </h1>
-    </Link>
+    <AnimateInView start="visible" animationClass="opacity-0 -translate-y-8">
+        <Link href="/" className="group">
+        <h1 className={cn('text-2xl font-bold tracking-tighter text-foreground transition-all duration-300 ease-in-out group-hover:text-glow')}>
+            Vijayaragavan
+        </h1>
+        </Link>
+    </AnimateInView>
   );
 }
